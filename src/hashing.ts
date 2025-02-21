e4cc06d9faefe43cf1434d284cc7b5b4c6b0f338
Ositha Dewruwan <dewruwanebay@gmail.com>
2025-02-21 13:23:26 +0530

    project build successfully

---
+++ src/hashing.ts
@@ -0,0 +1,30 @@
+import bcrypt from "bcrypt";
+import { TGenerateHashResult } from "./types";
+import generateSaltingText from "./salting";
+
+// Function to salt and pepper a password
+export default function generateHash(password: string): TGenerateHashResult {
+  // check if the environment variables are defined
+
+  if (!process.env.PEPPER_TEXT) {
+    throw new Error(
+      "PEPPER_TEXT is not defined! Please set it in the .env file."
+    );
+  }
+
+  // Load environment variables from .env file
+  const saltLength = parseInt(process.env.SALTING_TEXT_LENGTH || "5", 10);
+  const pepperText = process.env.PEPPER_TEXT;
+  const hashingRounds = parseInt(process.env.HASHING_ROUNDS || "10", 10);
+
+  // Generate a salt based on the configured length
+  const salt = generateSaltingText(saltLength);
+
+  // Combine the password with the pepper text
+  const saltedAndPepperedPassword = salt + password + pepperText;
+
+  // Hash the salted and peppered password with bcrypt
+  const hash = bcrypt.hashSync(saltedAndPepperedPassword, hashingRounds);
+
+  return { hash: hash, saltingText: salt };
+}
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+// type for output result of salt and pepper
+export type TGenerateHashResult = {
+  hash: string;
+  saltingText: string;
+};
